4f4d241f0bf679b2911e6cc778901d686e58260f
prathammittall <prathamv0106@gmail.com>
2026-03-25 14:50:03 +0530

    Remote proxy added

--- src/app/proxy.js
+++ src/app/proxy.js
@@ -1,7 +1,7 @@
 let USE_LOCAL_PATH=true;
 
 const LOCAL_PROXY = "http://localhost:4000";
-const REMOTE_PROXY = "";
+const REMOTE_PROXY = "https://boxed-bliss.onrender.com";
 
 const proxy = USE_LOCAL_PATH==true ? LOCAL_PROXY : REMOTE_PROXY;
 
