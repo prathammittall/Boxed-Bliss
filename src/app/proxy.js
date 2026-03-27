111806b0085702ff0a588922cb45bdd3e5f6180a
prathammittall <prathamv0106@gmail.com>
2026-03-27 22:25:38 +0530

    feat: Add admin authentication routes for login, logout, and user info, along with client-side proxy configuration.

--- src/app/proxy.js
+++
@@ -1,8 +0,0 @@
-let USE_LOCAL_PATH=false;
-
-const LOCAL_PROXY = "http://localhost:4000";
-const REMOTE_PROXY = "https://boxed-bliss.onrender.com";
-
-const proxy = USE_LOCAL_PATH==true ? LOCAL_PROXY : REMOTE_PROXY;
-
-export {proxy}
